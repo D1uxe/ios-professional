eacadb5e6013bf11309e2fcfb7329068c775d206
Artem <avbugro1@mts.ru>
2024-06-03 15:07:29 +0300

    add sui project

---
+++ ios-professional/ios-professional/ios_professionalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_professionalApp.swift
+//  ios-professional
+//
+//  Created by Бугров Артем on 03.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ios_professionalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
